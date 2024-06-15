25040a0b1c63e5e460859cd085155322298063ee
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-06-15 08:22:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ def tamilmv():
                 formatted_title = alltitles[p] if p < len(alltitles) else "Unknown Title"
                 formatted_filelink = filelink[p] if p < len(filelink) else "#"
                 real_dict[movie_list[num]].append(
-                    f"🧲 `{mag[p]}`\n*{formatted_title}* -->\n🗒️->[Torrent file]({formatted_filelink})"
+                    f"/qbleech `{mag[p]}`\n*{formatted_title}* -->\n🗒️->[Torrent file]({formatted_filelink})"
                 )
             except IndexError as e:
                 print(f"IndexError: {e}")
